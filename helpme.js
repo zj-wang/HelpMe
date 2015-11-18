ebccd3cdf9d8e7edb01343ea4117ca195116826b
zj-wang <jennywang@rochester.edu>
2015-11-18 02:48:36 -0500

    Milestone 3
    Added minimal job list and job functionality

--- helpme.js
+++ helpme.js
@@ -1,3 +1,6 @@
+Jobs = new Mongo.Collection('jobs');
+Lists = new Meteor.Collection('lists');
+
 if (Meteor.isClient) {
 
 	// when user wants to register
@@ -6,10 +9,10 @@ if (Meteor.isClient) {
 			event.preventDefault();
 			var email = $('[name=email]').val();
 			var password = $('[name=password]').val();
-			Accounts.createUser({		// Creates user based on email and password
+			Accounts.createUser({ // Creates user based on email and password
 				email : email,
 				password : password
-			}, function (error) {		
+			}, function (error) {
 				if (error) {
 					console.log(error.reason); // Output error if registration fails
 				} else {
@@ -35,34 +38,144 @@ if (Meteor.isClient) {
 			var email = $('[name=email]').val();
 			var password = $('[name=password]').val();
 
-			Meteor.loginWithPassword(email, password, function (error) {	// callback function with error handling
+			Meteor.loginWithPassword(email, password, function (error) { // callback function with error handling
 				if (error) {
 					console.log(error.reason);
 				} else {
-					Router.go("home");
+					var currentRoute = Router.current().route.getName();
+					if (currentRoute == "login") {
+						Router.go("home");
+					}
 				}
 			});
 		}
 	});
-	
+
+	Template.addList.events({
+		'submit form' : function (event) {
+			event.preventDefault();
+			var listName = $('[name=listName]').val();
+			var currentUser = Meteor.userId();
+			Lists.insert({
+				name : listName,
+				createdBy : currentUser
+			}, function (error, results) {
+				Router.go('listPage', {
+					_id : results
+				});
+			});
+			$('[name=listName]').val('');
+		}
+
+	});
+
+	Template.lists.helpers({
+		'list' : function () {
+			return Lists.find({}, {
+				sort : {
+					name : 1
+				}
+			});
+		}
+	});
+
 	Template.jobs.helpers({
-    'job': function(){
-        return jobs.find({}, {sort: {name: 1}});
-    }
-	
-});
+		'job' : function () {
+			var currentList = this._id;
+			return Jobs.find({
+				listId : currentList
+			}, {
+				sort : {
+					createdAt : -1
+				}
+			})
+		}
+	});
 
-	Template.addjob.events({
-    'submit form': function(event){
-      event.preventDefault();
-      var jobName = $('[name=jobName]').val();
-      jobs.insert({
-          name: jobName
-      });
-      $('[name=jobName]').val('');
-    }
-});
+	Template.addJob.events({
+		// events go here
 
+		'submit form' : function (event) {
+			event.preventDefault();
+			var jobName = $('[name="jobName"]').val();
+			var currentUser = Meteor.userId();
+			var currentList = this._id;
+			Jobs.insert({
+				name : jobName,
+				completed : false,
+				createdAt : new Date(),
+				createdBy : currentUser,
+				listId : currentList
+			});
+			$('[name="jobName"]').val('');
+		}
+
+	});
+
+	Template.jobItem.events({
+		// events go here
+
+		'click .delete-job' : function (event) {
+			event.preventDefault();
+			var documentId = this._id;
+			var confirm = window.confirm("Delete this job?");
+			if (confirm) {
+				Jobs.remove({
+					_id : documentId
+				});
+			}
+		},
+
+		'keyup [name=jobItem]' : function (event) {
+			if (event.which == 13 || event.which == 27) {
+				$(event.target).blur();
+			} else {
+				var documentId = this._id;
+				var jobItem = $(event.target).val();
+				Jobs.update({
+					_id : documentId
+				}, {
+					$set : {
+						name : jobItem
+					}
+				});
+			}
+		},
+
+		'change [type=checkbox]' : function () {
+			var documentId = this._id;
+			var isCompleted = this.completed;
+			if (isCompleted) {
+				Jobs.update({
+					_id : documentId
+				}, {
+					$set : {
+						completed : false
+					}
+				});
+			} else {
+				Jobs.update({
+					_id : documentId
+				}, {
+					$set : {
+						completed : true
+					}
+				});
+			}
+		}
+
+	});
+
+	Template.jobItem.helpers({
+		'checked' : function () {
+			var isCompleted = this.completed;
+			if (isCompleted) {
+				return "checked";
+			} else {
+				return "";
+			}
+		}
+	});
 
 }
 
@@ -80,18 +193,30 @@ Router.configure({
 
 // creating routes using Iron Router
 Router.route('/', {
-	name : 'home',		// label for the route
-	template : 'home'	// manually defining association with home template
+	name : 'home', // label for the route
+	template : 'home' // manually defining association with home template
 });
 
-Router.route('/register');	//auto associates route with template of same name
+Router.route('/register'); //auto associates route with template of same name
 Router.route('/login');
-Router.route('/job/:_id', {
-    template: 'jobPage',
-    data: function(){
-        var currentjob = this.params._id;
-        return jobs.findOne({ _id: currentjob });
-    }
+Router.route('/list/:_id', {
+	name : 'listPage',
+	template : 'listPage',
+	data : function () {
+		var currentList = this.params._id;
+		var currentUser = Meteor.userId();
+		return Lists.findOne({
+			_id : currentList,
+			createdBy : currentUser
+		});
+	},
+	onBeforeAction : function () {
+		var currentUser = Meteor.userId();
+		if (currentUser) {
+			this.next();
+		} else {
+			this.render("login");
+		}
+	}
 });
 
-Jobs = new Meteor.Collection('jobs');
